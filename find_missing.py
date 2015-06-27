af316b0ac100be65c9964eefbf406df9203c5642
changchengtu <tuchangcheng@TuChangchengs-MacBook-Air.local>
2015-06-28 00:53:20 +0700

    more

--- find_missing.py
+++ find_missing.py
@@ -27,8 +27,17 @@ import operator
 six_m = f.getForeignData(36)
 tmp = six_m.loc[(six_m['日期']<'2015-06-03'),:]
 names = tmp.columns.tolist()
+
+exception = ['鋒裕基金-亞洲股票(不含日本)U2','鋒裕基金-新興市場債券A2','鋒裕基金-新興歐洲及地中海股票U2',
+			'鋒裕基金-新興歐洲及地中海股票U2(歐元)','鋒裕基金-核心歐洲股票A2(歐元)','鋒裕基金-歐洲潛力A2','鋒裕基金-環球生態U2',
+			'鋒裕基金-美元短期債券U2','鋒裕基金-新興市場債券T2(歐元)','鋒裕基金-新興市場當地貨幣債券UXD','匯豐環球投資基金-巴西股票ID',
+			'匯豐環球投資基金-歐元區股票ID','匯豐環球投資基金-歐洲股票ID','匯豐環球投資基金-環球債券IC',
+			'霸菱德國增長基金- A類美元避險累積型','霸菱俄羅斯基金-A類美元累積型']
+
 missing_n = []
 for n in names:
+	if n in exception:
+		continue
 	tmp_list = tmp[n]
 	state = False
 	for v in tmp_list:
